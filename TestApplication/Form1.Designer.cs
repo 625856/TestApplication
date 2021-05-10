014dca157c51792f938b1ad75709946d37a7d7b3
murthii <murthykagita@gmail.com>
2021-05-10 20:24:15 +0530

    Initial Checkin

---
+++ TestApplication/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace TestApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDriver = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtdistance = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ddlDrivers = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.btnReport = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDriver
+            // 
+            this.lblDriver.AutoSize = true;
+            this.lblDriver.Location = new System.Drawing.Point(66, 56);
+            this.lblDriver.Name = "lblDriver";
+            this.lblDriver.Size = new System.Drawing.Size(38, 15);
+            this.lblDriver.TabIndex = 0;
+            this.lblDriver.Text = "Driver";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(116, 48);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(171, 23);
+            this.txtName.TabIndex = 1;
+            this.txtName.Leave += new System.EventHandler(this.txtName_Leave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Trip";
+            // 
+            // txtdistance
+            // 
+            this.txtdistance.Location = new System.Drawing.Point(516, 94);
+            this.txtdistance.Name = "txtdistance";
+            this.txtdistance.Size = new System.Drawing.Size(83, 23);
+            this.txtdistance.TabIndex = 5;
+            this.txtdistance.Leave += new System.EventHandler(this.txtdistance_Leave);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(596, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "mph";
+            // 
+            // ddlDrivers
+            // 
+            this.ddlDrivers.AllowDrop = true;
+            this.ddlDrivers.FormattingEnabled = true;
+            this.ddlDrivers.Location = new System.Drawing.Point(116, 94);
+            this.ddlDrivers.Name = "ddlDrivers";
+            this.ddlDrivers.Size = new System.Drawing.Size(121, 23);
+            this.ddlDrivers.TabIndex = 2;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(256, 94);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(124, 23);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(386, 94);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(124, 23);
+            this.dateTimePicker2.TabIndex = 4;
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Location = new System.Drawing.Point(66, 140);
+            this.txtDisplay.Multiline = true;
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(533, 128);
+            this.txtDisplay.TabIndex = 6;
+            // 
+            // btnReport
+            // 
+            this.btnReport.Location = new System.Drawing.Point(66, 291);
+            this.btnReport.Name = "btnReport";
+            this.btnReport.Size = new System.Drawing.Size(118, 23);
+            this.btnReport.TabIndex = 7;
+            this.btnReport.Text = "Generate Report";
+            this.btnReport.UseVisualStyleBackColor = true;
+            this.btnReport.Click += new System.EventHandler(this.btnReport_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(190, 291);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(272, 291);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "Exit";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnReport);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.ddlDrivers);
+            this.Controls.Add(this.txtdistance);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDisplay);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblDriver);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDriver;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtdistance;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox ddlDrivers;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.Button btnReport;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ TestApplication/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string strDisplay { get; set; }
+
+        public double totalHours { get; set; }
+
+        public decimal speed { get; set; }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "MM/dd/yyyy hh:mm:ss";
+
+            dateTimePicker2.Format = DateTimePickerFormat.Custom;
+            dateTimePicker2.CustomFormat = "MM/dd/yyyy hh:mm:ss";
+        }
+
+        private void txtName_Leave(object sender, EventArgs e)
+        {
+            if (txtName.Text == string.Empty)
+            {
+                MessageBox.Show("Driver name should not be empty!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtName.Focus();
+                return;
+            }
+
+            ddlDrivers.Text = txtName.Text;
+            ddlDrivers.Items.Add(txtName.Text);
+        }
+
+        private void txtdistance_Leave(object sender, EventArgs e)
+        {
+            if (dateTimePicker1.Value > dateTimePicker2.Value)
+            {
+                MessageBox.Show("End Date must be greater than Start Date!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dateTimePicker1.Focus();
+                return;
+            }
+
+            if (dateTimePicker2.Value < dateTimePicker1.Value)
+            {
+                MessageBox.Show("End Date must be greater than Start Date!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dateTimePicker2.Focus();
+                return;
+            }
+
+
+            if (ddlDrivers.Text == string.Empty || ddlDrivers.Text == null)
+            {
+                MessageBox.Show("No Driver assigned for this Trip!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            if (txtdistance.Text == string.Empty)
+                txtdistance.Text = "0";
+
+
+            totalHours = (dateTimePicker1.Value - dateTimePicker2.Value).Duration().TotalHours;
+
+            if (totalHours > 0)
+                speed = Math.Round(Convert.ToDecimal(Convert.ToDouble(txtdistance.Text) / totalHours), 2);
+            else
+                speed = 0;
+
+            if (txtName.Text != string.Empty)
+            {
+                DialogResult dialogResult = MessageBox.Show("Would you like to continue?", "Confirmation", MessageBoxButtons.YesNo);
+
+                if (dialogResult == DialogResult.Yes)
+                {
+                    strDisplay = ddlDrivers.Text + ":" + " " + txtdistance.Text + " Miles" + " @ " + speed.ToString() + " mph";
+
+                    if (txtDisplay.Text == string.Empty)
+                        txtDisplay.Text = strDisplay;
+                    else
+                        txtDisplay.Text = txtDisplay.Text + Environment.NewLine + strDisplay;
+                }
+               
+            }
+
+        }
+
+        private void btnReport_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "*.txt|*.txt";
+            dlg.RestoreDirectory = true;
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(dlg.FileName, txtDisplay.Text);
+
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtName.Clear();
+            txtdistance.Clear();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.ExitThread();
+        }
+    }
+}
